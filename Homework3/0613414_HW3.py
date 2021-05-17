8757ca963df5fe7d794299c50ef5ab0ae6232ca8
Cheng Che <chengche6230@gmail.com>
2021-05-18 01:46:39 +0800

    Random forest still has some bugs

--- Homework3/0613414_HW3.py
+++ Homework3/0613414_HW3.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.metrics import accuracy_score
 
-
 _class = [0, 1]
 train_num = 426
 test_num = 143
@@ -58,6 +57,36 @@ class Tree():
         print('----------------------------')
         print(f'#:{len(self.x)} Depth:{self.depth}, isLeaf:{self.isLeaf}, info:{self.info}')
         print(np.sum(self.x['label']==0),np.sum(self.x['label']==1))
+
+def splitAttr(x, info, criterion):
+    attr_index, threshold = 0, 0.0
+    info_gain = 0
+    for col in range(len(x.columns)-1): # exclude label column
+        tmp_x = x.sort_values(by=[x.columns[col]])
+        for i in range(len(x)):
+            left = impurity(tmp_x.values[0:i+1, -1], criterion)
+            right = impurity(tmp_x.values[i:train_num, -1], criterion)
+            info_p = ((i + 1) * left + (train_num - i + 1) * right) / train_num
+            if info - info_p > info_gain:
+                info_gain = info - info_p
+                attr_index, threshold = col, tmp_x.values[i, col]
+    return info_gain, attr_index, threshold
+    
+def partiTree(tree, attr_index, threshold):
+    left_df = pd.DataFrame(columns=tree.x.columns)
+    right_df = pd.DataFrame(columns=tree.x.columns)
+    for i in range(len(tree.x)):
+        if tree.x.values[i, attr_index] <= threshold:
+            left_df = left_df.append(tree.x.iloc[i])
+        else:
+            right_df = right_df.append(tree.x.iloc[i])
+    left = Tree(left_df, tree.depth + 1)
+    right = Tree(right_df, tree.depth + 1)
+    if len(left.x) <= 0:
+        left.isLeaf = True
+    if len(right.x) <= 0:
+        right.isLeaf = True
+    return left, right
 
 class DecisionTree():
     def __init__(self, criterion='gini', max_depth=None):
@@ -166,7 +195,90 @@ def visualize(importance):
     
 class RandomForest():
     def __init__(self, n_estimators, max_features, boostrap=True, criterion='gini', max_depth=None):
-        return None
+        self.n_estimators = n_estimators
+        self.max_features = int(max_features)
+        self.boostrap = boostrap
+        self.criterion = criterion
+        self.max_depth = 10
+        self.forest = []
+        
+    def featureDelete(self):
+        selected = np.random.choice(feature_num, self.max_features, replace=False)
+        return np.delete(np.arange(feature_num), selected)
+    
+    def growTree(self, tree):
+        # Init tree info.
+        tree.info = impurity(tree.x['label'], self.criterion)
+        #input()
+        #tree.Print()
+        if tree.depth >= self.max_depth or tree.info == 0:
+            tree.isLeaf = True
+            return tree
+        
+        # Randomly select m attribute
+        tmp_df = tree.x.copy()
+        delete_feature = self.featureDelete()
+        tmp_df = tmp_df.drop(columns=[feature_name[i] for i in delete_feature])
+        #print(tmp_df)
+        
+        # Select best split attribute and threshold
+        info_gain, attr_index, threshold = splitAttr(tmp_df, tree.info, self.criterion)
+        #print(f'Depth:{tree.depth}, {info_gain}, at {attr_index}({feature_name[attr_index]})')
+        if info_gain == 0:
+            tree.isLeaf = True
+            return tree
+        
+        # Partition tree
+        left, right = partiTree(tree, attr_index, threshold)
+        tree.setting(attr_index, threshold, left, right)
+        
+        # Move to tree's children
+        if not left.isLeaf:
+            #print('In left')
+            self.growTree(left)
+        if not right.isLeaf:
+            #print('In right')
+            self.growTree(right)
+        return tree
+    
+    def Bagging(self, X):
+        df = pd.DataFrame(columns=X.columns)
+        for n in range(len(X)):
+            index = np.random.randint(0, len(X))
+            df = df.append(X.loc[index])
+        return df
+    
+    def buildForest(self, X):
+        for n in range(self.n_estimators):
+            data = self.Bagging(X) if self.boostrap else X
+            root = Tree(data, 1)
+            self.forest.append(self.growTree(root))
+    
+    def model(self, test):
+        vote = np.zeros((len(_class)), dtype=np.uint32)
+        for n in range(self.n_estimators):
+            tmp = self.forest[n]
+            while True:
+                if test[tmp.attr_index] <= tmp.threshold:
+                    if tmp.left == None:
+                        break
+                    tmp = tmp.left
+                else:
+                    if tmp.right == None:
+                        break
+                    tmp = tmp.right
+            pre = np.zeros((len(_class)), dtype=np.uint32)
+            for c in range(len(_class)):
+                pre[c] = np.sum(tmp.x['label']==_class[c])
+            vote[np.argmax(pre)] += 1
+        #print(vote)
+        return _class[np.argmax(vote)]
+        
+    def predict(self, X):
+        pred_y = np.zeros((test_num), dtype=np.uint8)
+        for i in range(test_num):
+            pred_y[i] = self.model(X.values[i])  
+        return pred_y
 
 if __name__ == "__main__":
     #data = load_breast_cancer()
@@ -182,14 +294,22 @@ if __name__ == "__main__":
     train = train.rename(columns={'0': 'label'})
     feature_name = [s for s in x_train.columns]
     
-    print("--------------------------------------------")
+    
+    tmp = RandomForest(n_estimators=10, max_features=np.sqrt(x_train.shape[1]))
+    tmp.buildForest(train)
+    y_pred = tmp.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"# of estimoators = 10, Maximum # of features = 5, Acc: {acc}")
+    
+    '''
+    print("------------------------------------------------------")
     # Question 1
     print("Question 1")
     data = np.array([1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0])
     print("Gini of data is ", gini(data))
     print("Entropy of data is ", entropy(data))
     
-    print("--------------------------------------------")
+    print("------------------------------------------------------")
     # Question 2.1
     print("Question 2.1")
     clf_depth3 = DecisionTree(criterion='gini', max_depth=3)
@@ -204,7 +324,7 @@ if __name__ == "__main__":
     acc = accuracy_score(y_test, y_pred)
     print("Criterion = Gini, Max Depth = 10, Acc:", acc)
     
-    print("--------------------------------------------")
+    print("------------------------------------------------------")
     # Question 2.2
     print("Question 2.2")
     clf_gini = DecisionTree(criterion='gini', max_depth=3)
@@ -219,16 +339,40 @@ if __name__ == "__main__":
     acc = accuracy_score(y_test, y_pred)
     print("Criterion = Entropy, Max Depth = 3, Acc:", acc)    
     
-    print("--------------------------------------------")
+    print("------------------------------------------------------")
     # Question 3: Visualize
     print("Question 3: visualize as plot")
     clf_depth10.countImportance(clf_depth10.tree)
     visualize(clf_depth10.importance)
-    
     '''
+    '''
+    print("------------------------------------------------------")
+    # Question 4.1: Random Forest
     clf_10tree = RandomForest(n_estimators=10, max_features=np.sqrt(x_train.shape[1]))
-    clf_100tree = RandomForest(n_estimators=100, max_features=np.sqrt(x_train.shape[1]))
+    clf_10tree.buildForest(train)
+    y_pred = clf_10tree.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"# of estimoators = 10, Maximum # of features = 5, Acc: {acc}")
     
+    clf_100tree = RandomForest(n_estimators=100, max_features=np.sqrt(x_train.shape[1]))
+    clf_100tree.buildForest(train)
+    y_pred = clf_100tree.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"# of estimoators = 100, Maximum # of features = 5, Acc: {acc}")    
+    
+    print("------------------------------------------------------")
+    # Question 4.2
     clf_random_features = RandomForest(n_estimators=10, max_features=np.sqrt(x_train.shape[1]))
+    clf_random_features.buildForest(train)
+    y_pred = clf_random_features.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"# of estimoators = 10, Maximum # of features = 5, Acc: {acc}")
+    
     clf_all_features = RandomForest(n_estimators=10, max_features=x_train.shape[1])
+    clf_all_features.buildForest(train)
+    y_pred = clf_all_features.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"# of estimoators = 10, Maximum # of features = 30, Acc: {acc}")
+    print("------------------------------------------------------")
     '''
+    
